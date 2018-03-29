f24ac44fbbf06223b5bf1ba28ea0fba057181126
mswang14 <35810775+mswang14@users.noreply.github.com>
2018-03-29 14:16:56 -0400

    Add files via upload

--- image_analysis/autogenerate_connectivity.m
+++ image_analysis/autogenerate_connectivity.m
@@ -3,13 +3,13 @@ runnos = {'N54717','N54718','N54719','N54720','N54722','N54759','N54760','N54761
 
 for i=1:numel(runnos)
     
-    cmd = ['/Applications/dsi_studio1.app/Contents/MacOS/dsi_studio --action=trk --source=/Volumes/CivmUsers/omega/alex/E3E4v1/connect4dsistudio/pre_rigid_native_space/' runnos{i} '/' runnos{i} '_nii4D_RAS.nii.gz.src.gz.dti.fib.gz --method=1 --fiber_count=2000000 --fa_threshold=0.08 --initial_dir=1 --seed_plan=0 --interpolation=0 --thread_count=12 --random_seed=1 --step_size=0.03 --smoothing=0.01 --min_length=0.0 --max_length=300.0 --connectivity=/Volumes/CivmUsers/omega/alex/E3E4v1/connect4dsistudio/pre_rigid_native_space/fa_labels_warp_' runnos{i} '_RAS.nii.gz --connectivity_value=count --connectivity_threshold=0 --connectivity_type=pass > /Volumes/CivmUsers/omega/alex/E3E4v1/connect4dsistudio/pre_rigid_native_space/' runno{i} '/' runno{i} '.log'];
-    [res,msg]=system(cmd,-echo);
+    cmd1 = ['/Applications/dsi_studio1.app/Contents/MacOS/dsi_studio --action=trk --source=/Volumes/CivmUsers/omega/alex/E3E4v1/connect4dsistudio/pre_rigid_native_space/' runnos{i} '/' runnos{i} '_nii4D_RAS.nii.gz.src.gz.dti.fib.gz --method=1 --fiber_count=2000000 --fa_threshold=0.08 --initial_dir=1 --seed_plan=0 --interpolation=0 --thread_count=12 --random_seed=1 --step_size=0.03 --smoothing=0.01 --min_length=0.0 --max_length=300.0 --connectivity=/Volumes/CivmUsers/omega/alex/E3E4v1/connect4dsistudio/pre_rigid_native_space/fa_labels_warp_' runnos{i} '_RAS.nii.gz --connectivity_value=count --connectivity_threshold=0 --connectivity_type=pass > /Volumes/CivmUsers/omega/alex/E3E4v1/connect4dsistudio/pre_rigid_native_space/' runnos{i} '/' runnos{i} '.log'];
+    [res,msg]=system(cmd1,'-echo');
     
 end
 
 
-/Applications/dsi_studio1.app/Contents/MacOS/dsi_studio --action=trk --source=/Volumes/CivmUsers/omega/alex/E3E4v1/connect4dsistudio/pre_rigid_native_space/N54764/N54764_nii4D_RAS.nii.gz.src.gz.dti.fib.gz --method=1 --fiber_count=2000000 --fa_threshold=0.08 --initial_dir=1 --seed_plan=0 --interpolation=0 --thread_count=12 --random_seed=1 --step_size=0.03 --smoothing=0.01 --min_length=0.0 --max_length=300.0 --connectivity=/Volumes/CivmUsers/omega/alex/E3E4v1/connect4dsistudio/pre_rigid_native_space/fa_labels_warp_N54764_RAS.nii.gz --connectivity_value=count --connectivity_threshold=0 --connectivity_type=pass > /Volumes/CivmUsers/omega/alex/E3E4v1/connect4dsistudio/pre_rigid_native_space/N54764/N54764.log
+% /Applications/dsi_studio1.app/Contents/MacOS/dsi_studio --action=trk --source=/Volumes/CivmUsers/omega/alex/E3E4v1/connect4dsistudio/pre_rigid_native_space/N54764/N54764_nii4D_RAS.nii.gz.src.gz.dti.fib.gz --method=1 --fiber_count=2000000 --fa_threshold=0.08 --initial_dir=1 --seed_plan=0 --interpolation=0 --thread_count=12 --random_seed=1 --step_size=0.03 --smoothing=0.01 --min_length=0.0 --max_length=300.0 --connectivity=/Volumes/CivmUsers/omega/alex/E3E4v1/connect4dsistudio/pre_rigid_native_space/fa_labels_warp_N54764_RAS.nii.gz --connectivity_value=count --connectivity_threshold=0 --connectivity_type=pass > /Volumes/CivmUsers/omega/alex/E3E4v1/connect4dsistudio/pre_rigid_native_space/N54764/N54764.log
 
 
 
